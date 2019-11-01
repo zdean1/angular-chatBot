0c9545f16ccc74576aca1576d5c6a47fcca5ccc1
Zach Dean <zdean3721@gmail.com>
2019-11-01 18:14:23 -0500

    added chat capabilities

--- src/app/chat/chat-dialog/chat-dialog.component.ts
+++ src/app/chat/chat-dialog/chat-dialog.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService, Message } from '../chat.service';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/scan';
 
 @Component({
-  selector: 'app-chat-dialog',
+  selector: 'chat-dialog',
   templateUrl: './chat-dialog.component.html',
   styleUrls: ['./chat-dialog.component.css']
 })
 export class ChatDialogComponent implements OnInit {
 
-  constructor() { }
+  messages: Observable<Message[]>;
+  formValue: string;
+
+  constructor(public _chatService: ChatService) { }
 
   ngOnInit() {
+    this.messages = this._chatService.conversation.asObservable()
+        .scan((acc, val) => acc.concat(val) );
   }
 
+  sendMessage() {
+    this._chatService.converse(this.formValue);
+    this.formValue = '';
+  }
 }
--- src/app/chat/chat.service.ts
+++ src/app/chat/chat.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+import { ApiAiClient } from 'api-ai-javascript';
+
+import { Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
+
+export class Message {
+  constructor(public content: string, public sentBy: string) {}
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
+  readonly token = environment.dialogflow.angularBot;
+  readonly client = new ApiAiClient({ accessToken: this.token });
+
+  conversation = new BehaviorSubject<Message[]>([]);
+
   constructor() { }
+
+  converse(msg: string) {
+    const userMessage = new Message(msg, 'user');
+    this.update(userMessage);
+
+    return this.client.textRequest(msg)
+               .then(res => {
+                  const speech = res.result.fulfillment.speech;
+                  const botMessage = new Message(speech, 'bot');
+                  this.update(botMessage);
+               });
+  }
+
+  update(msg: Message) {
+    this.conversation.next([msg]);
+  }
 }
