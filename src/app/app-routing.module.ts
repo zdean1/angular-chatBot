d7ed24b9326f129129cda266378c97e56c469127
Zach Dean <zdean3721@gmail.com>
2019-11-01 17:18:51 -0500

    added chat module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChatModule } from './chat/chat.module';
 
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    ChatModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
